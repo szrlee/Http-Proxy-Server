2fa221847a4d6bcc6bcd7a2667a84cbcf2b07c78
szrlee <szrlee@gmail.com>
2014-01-23 02:03:53 +0000

    ssl无力 感觉被坑了

--- proxy.py
+++ proxy.py
@@ -10,26 +10,29 @@ def main():
     host = ''
     port = 8089
     try:
-        proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        proxy.bind((host, port))
-        proxy.listen(backlog)
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((host, port))
+        server.listen(backlog)
     except socket.error, (value, message):
-        if proxy:
-            proxy.close()
+        if server:
+            server.close()
         print "Could not open socket:", message
         sys.exit(1)
 
     while 1:
-        conn, client_addr = proxy.accept()
+        conn, client_addr = server.accept()
         #create a BP socket
-        thread.start_new_thread(BPS, (proxy, conn, client_addr))
+        thread.start_new_thread(BPS, (server, conn, client_addr))
             
-    proxy.close()
+    server.close()
 
-def BPS(proxy, conn, client_addr):
+
+def BPS(server, conn, client_addr):
     request = conn.recv(MAX_DATA_RECV)
+    print request
     got = getaddr_port(request)
     (firstline, webserver, port) = (got[0],got[1],got[2])
+    print webserver, port
     print firstline
     print client_addr 
     thread.start_new_thread(SPB,(request, conn, webserver, port))
@@ -37,22 +40,36 @@ def BPS(proxy, conn, client_addr):
     
 def SPB(request, conn, webserver, port):
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((webserver, port))
-        s.send(request)
+        proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        proxy.connect((webserver, port))
+        #ssl_conn = 'CONNECT %s:%s HTTP/1.1\r\n' % (webserver, port)
+        #useragent = 'User-Agent: Mozilla/5.0\r\n'
+        #proxy_connection = 'Proxy-Connection: Keep-alive\r\n'
+        #connection = 'Connection: keep-alive\r\n'
+        #Host = 'Host: %s\r\n' % webserver
+        #proxy_pieces = ssl_conn+useragent+proxy_connection+connection+Host+'\r\n'
+        #print proxy_pieces
+        #proxy.sendall(proxy_pieces+'\r\n')
+        #response = proxy.recv(MAX_DATA_RECV)
+       # status = response.split(None, 1)[1]
+       # if int(status)/100 !=2:
+        #    print 'error', response 
+         #   raise RuntimeError(status)
+       # else:
+        proxy.sendall(request)
 
         while 1:
-            data = s.recv(MAX_DATA_RECV)
+            data = proxy.recv(MAX_DATA_RECV)
 
             if (len(data) > 0):
                 conn.send(data)
             else:
                 break
-        s.close()
+        proxy.close()
         conn.close()
     except socket.error, (value, message):
-        if s:
-            s.close()
+        if proxy:
+            proxy.close()
         if conn:
             conn.close()
         print ('peer reset')
@@ -75,12 +92,12 @@ def getaddr_port(request):
     webserver_pos = temp.find('/')
 
     if webserver_pos== -1:
-        length = len(temp)
+        webserver_pos = len(temp)
    
     webserver = ''
     port = -1
 
-    if port_pos == -1 or length < port_pos:
+    if port_pos == -1 or webserver_pos < port_pos:
         webserver = temp[:webserver_pos]
         port = 80
     else:
